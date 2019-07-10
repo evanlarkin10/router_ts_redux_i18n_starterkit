f480157f8d582366bae27d7edeee88acef50533e
Evan Larkin <elarkin@cafrdm1079280.ca.deloitte.com>
2019-07-10 15:10:50 -0300

    API

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import "./App.css";
 // import Routes from "./routes";
 import { hot } from "react-hot-loader/root";
 import Authentication from "components/auth/authenticator";
-import Amplify from 'aws-amplify'
+import Amplify, { API, graphqlOperation, Auth } from 'aws-amplify'
+import * as queries from "./graphql/queries";
+import * as mutations from "./graphql/mutations";
 import awsconfig from './aws-exports';
 Amplify.configure({
   ...awsconfig,
@@ -13,8 +15,28 @@ Amplify.configure({
   }
 });
 class App extends React.Component {
-  // get current authenticated user
 
+  componentDidMount() {
+    console.log("comp mount tsx")
+    API.graphql(graphqlOperation(queries.getUser))
+      .then(async (user: any) => {
+        console.log("USER:", user)
+        if (user.data.getUser == null) {
+          const identityUser = await Auth.currentCredentials();
+          const identityId = identityUser.identityId;
+
+          const input = {
+            identity_id: identityId,
+          };
+
+          API.graphql(graphqlOperation(mutations.createUser, { input }))
+
+        }
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  }
   render() {
     return <Authentication />;
   }
---
+++ src/graphql/queries.ts
@@ -0,0 +1,70 @@
+// tslint:disable
+// this is an auto generated file. This will be overwritten
+
+export const getTask = `query GetTask($id: ID!) {
+  getTask(id: $id) {
+    id
+    title
+    description
+    status
+  }
+}
+`;
+export const listTasks = `query ListTasks(
+  $filter: ModelTaskFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listTasks(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      title
+      description
+      status
+    }
+    nextToken
+  }
+}
+`;
+export const getPrivateNote = `query GetPrivateNote($id: ID!) {
+  getPrivateNote(id: $id) {
+    id
+    content
+  }
+}
+`;
+export const listPrivateNotes = `query ListPrivateNotes(
+  $filter: ModelPrivateNoteFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listPrivateNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      content
+    }
+    nextToken
+  }
+}
+`;
+export const getUser = `query GetUser($id: ID!) {
+  getUser(id: $id) {
+    id
+    identity_id
+  }
+}
+`;
+export const listUsers = `query ListUsers(
+  $filter: ModelUserFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      identity_id
+    }
+    nextToken
+  }
+}
+`;
